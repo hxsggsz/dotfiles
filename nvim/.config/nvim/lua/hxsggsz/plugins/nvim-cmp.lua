c1bb203da890463f442339a2d5db175f0648ab41
hxsggsz <vt.hugo.2021@gmail.com>
2025-04-15 13:05:30 -0300

    update lsp autocomplete

--- nvim/.config/nvim/lua/hxsggsz/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/hxsggsz/plugins/nvim-cmp.lua
@@ -1,63 +1,114 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		{
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "InsertEnter" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"brenoprata10/nvim-highlight-colors",
+			"saadparwaiz1/cmp_luasnip",
+			"onsails/lspkind.nvim",
 			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
 		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
-	config = function()
-		local cmp = require("cmp")
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local format_item_with_lspkind = require("lspkind").cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50,
+				ellipsis_char = "...",
+				menu = {
+					nvim_lsp = "[LSP]",
+					buffer = "[Buffer]",
+					path = "[Path]",
+					luasnip = "[Snippet]",
+					nvim_lsp_signature_help = "[Signature]",
+				},
+			})
 
-		local luasnip = require("luasnip")
+			luasnip.config.setup({})
 
-		local lspkind = require("lspkind")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				completion = { completeopt = "menu,menuone,noinsert" },
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable(1) then
+							luasnip.expand_or_jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
-			-- sources for autocompletion
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-e>"] = cmp.mapping.abort(), -- close completion window
+					["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scroll up preview
+					["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll down preview
+					["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+					["<CR>"] = cmp.mapping(function(fallback) -- select suggestion or snippet
+						if cmp.visible() then
+							if luasnip.expandable() then
+								luasnip.expand()
+							else
+								cmp.confirm({
+									select = true,
+								})
+							end
+						else
+							fallback()
+						end
+					end),
 				}),
-			},
-		})
-	end,
+
+				-- sources for autocompletion
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp", group_index = 1 }, -- lsp
+					{ name = "buffer", max_item_count = 5, group_index = 2 }, -- text within current buffer
+					{ name = "path", max_item_count = 3, group_index = 3 }, -- file system paths
+					{ name = "luasnip", max_item_count = 3, group_index = 5 }, -- snippets
+					{ name = "nvim-lsp-signature-help" },
+				}),
+				-- Enable pictogram icons for lsp/autocompletion
+				formatting = {
+					expandable_indicator = true,
+					format = function(entry, item)
+						local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+						item = format_item_with_lspkind(entry, item)
+
+						if color_item.abbr_hl_group then
+							item.kind_hl_group = color_item.abbr_hl_group
+							item.kind = color_item.abbr
+						end
+
+						return item
+					end,
+				},
+				experimental = {
+					ghost_text = true,
+				},
+			})
+		end,
+	},
 }
